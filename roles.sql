91de689d2b1d0e87d09337b39fa9c61ce5087171
dstoykov <maverick1997@abv.bg>
2018-05-10 23:13:27 +0300

    Added roles.sql

---
+++ roles.sql
@@ -0,0 +1,3 @@
+INSERT INTO roles(id, authority) VALUES ('d3aba3fd-0bb3-4f54-884b-4a91e6e6fbdc', 'ADMIN'),
+ ('ca253cbf-a300-41b8-da21-0880ee0b0b95', 'USER'), 
+ ('4c90df64-941b-48be-d562-57050c5a86e9', 'MODERETOR');
